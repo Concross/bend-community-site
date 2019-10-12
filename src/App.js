01efcb3451e67dce0fe26943ea9b1792296821e1
Concross <cacrossley22@gmail.com>
2019-10-11 19:35:22 -0700

    remove space on end of line

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
-=======
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
->>>>>>> 34a7a06133eebcc446e99c76849f271d4dbef2b9
 
 function App() {
   return (
